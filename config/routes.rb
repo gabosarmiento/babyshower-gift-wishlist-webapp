8983a975a360f1f1584dfbd72ec407c341bf271c
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-05-16 20:32:06 -0500

    Cambiar vista de editar usuario

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Eclist::Application.routes.draw do
   get "registrarse", to: "devise/registrations#new"
   end
   devise_for :users
+  resources :users, only: [:update]
   get "inicio" => "bienvenida#inicio"
   root to: 'bienvenida#inicio'
 
