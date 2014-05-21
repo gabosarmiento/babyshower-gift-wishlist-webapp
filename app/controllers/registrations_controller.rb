e0beebfb22aacf3a02a133f1a8973d8d04bc8bdb
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-05-20 22:01:39 -0500

    Saltar la confirmación del correo y redirigir a la fiesta, cambio de diseño

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,24 +9,28 @@ class RegistrationsController < Devise::RegistrationsController
     @token = params[:convidado_token]
 
     build_resource(sign_up_params)
-
+    resource.skip_confirmation!
     resource_saved = resource.save
     yield resource if block_given?
     if resource_saved
       if @token != nil
         convidado = Convidado.find_by_token(@token)
-        # org =  convidado.fiesta #encuentra la fiesta atada a la invitación
+        org =  convidado.fiesta #encuentra la fiesta atada a la invitación
         # resource.fiestas.push(org) #añade este usuario a la fiesta como rsvp
         resource.invitaciones.push(convidado)
-      end
-      if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_flashing_format?
         sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
+        respond_with resource, location: fiesta_path(org)
       else
-        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
-        expire_data_after_sign_in!
-        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_flashing_format?
+          sign_up(resource_name, resource)
+          respond_with resource, location: after_sign_up_path_for(resource)
+        else
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
+          expire_data_after_sign_in!
+          respond_with resource, location: after_inactive_sign_up_path_for(resource)
+        end
       end
     else
       clean_up_passwords resource
