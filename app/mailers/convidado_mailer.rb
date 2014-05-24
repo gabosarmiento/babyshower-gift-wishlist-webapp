a6f916843b73939ce78ab9b797eae56524967c48
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-05-24 15:42:24 -0500

    Cambiar el mailer

--- app/mailers/convidado_mailer.rb
+++ app/mailers/convidado_mailer.rb
@@ -1,9 +1,9 @@
 class ConvidadoMailer < ActionMailer::Base
-  default from: "no-reply@lalista.herokuapp.com"
+  default from: "invitaciones@lalista.herokuapp.com"
   def nuevo_usuario_convidado(record, url)
     @convidado = record
     @url = url
-    mail(to: @convidado.email, subject: "Has recibido una invitación de #{User.find(@convidado.anfitrion_id).name.to_s.humanize.titleize}")
+    mail(to: @convidado.email, subject: "#{User.find(@convidado.anfitrion_id).name.to_s.humanize.titleize} te invita al #{@convidado.fiesta.nombre.to_s.humanize.titleize}")
   end
 
   def usuario_existente_convidado(record)
