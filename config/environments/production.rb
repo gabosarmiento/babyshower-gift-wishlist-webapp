c99406a761721ecc814dc931f24a6348430a78fb
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-05-30 08:55:31 -0500

    ajustar mail para produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Eclist::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'www.lalistaderegalos.co' }  
+  config.action_mailer.default_url_options = { :host => 'lalistaderegalos.co' }  
 end
