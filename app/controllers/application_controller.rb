c3bdfa45cd0990a14113e1392c8ff3ae1bc11ed1
Gabosarmiento <gabrielsarmiento@gmail.com>
2014-06-03 22:11:33 -0500

    Corregir controllador compromisos y JS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  before_filter :authenticate_user!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
